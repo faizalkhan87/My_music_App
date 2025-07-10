9394f4da5376a02e568cb4fe0de38a15618f6d82
shakir-manzoor <shakirmanzoor233@gmail.com>
2025-07-10 12:41:03 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "Music App",
+  base: "My_music_App",
 });
